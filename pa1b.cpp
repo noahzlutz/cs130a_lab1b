e46d591dbf8b4a3d1b163628eedda3f2c7a36d31
Noah Lutz <59719300+noahzlutz@users.noreply.github.com>
2021-10-25 21:34:54 -0700

    Add files via upload

--- pa1b.cpp
+++ pa1b.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 int main( int argc, char** argv ){
 
-    Dictionary dict = Dictionary(argv[1], argv[2]);
+    Dictionary dict = Dictionary(argv[2], argv[1]);
     cout<< "Number of words = " << dict.get_input_size() << endl;
     cout<< "Table size = " << dict.get_primary_arr_size() << endl;
     cout<< "Max collisions = " << dict.max_num_collision() << endl;
-    
+
 
 
 }
